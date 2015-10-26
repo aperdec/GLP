5296acf853d0d1dc5f0a682c79c124e8397666f6
alecalec2 <alec_z@live.com>
2015-10-26 15:48:38 -0400

    question json

---
+++ app/data/aspnet.js
@@ -0,0 +1,117 @@
+{
+    "quiz": {
+        "Id": 1,
+        "name": "GLP Survey",
+        "description": "A serious of questions to enhance your GLP experience"
+    },
+    "questions": [{
+        "Id": 1010,
+        "Name": "Question 1 on ___________________________", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "a1", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "a2", "IsAnswer": true },
+            { "Id": 1057, "QuestionId": 1010, "Name": "a3", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "None of the above", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1011,
+        "Name": "Question 2 on ___________________________", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "b1", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "b2", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "b3", "IsAnswer": true },
+            { "Id": 1058, "QuestionId": 1010, "Name": "None of the above", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1012,
+        "Name": "Question 3 on ___________________________", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "c1", "IsAnswer": true },
+            { "Id": 1057, "QuestionId": 1010, "Name": "c2", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "c3", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "c4", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1013,
+        "Name": "Question 4 on ___________________________", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "d1", "IsAnswer": true },
+            { "Id": 1057, "QuestionId": 1010, "Name": "d2", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "d3", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "d4", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1014,
+        "Name": "Question 5 on ___________________________", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "e1", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "e2", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "e3", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "e4", "IsAnswer": true }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1015,
+        "Name": "Question 6 on ___________________________", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "f417", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "f418", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "f419", "IsAnswer": true },
+            { "Id": 1058, "QuestionId": 1010, "Name": "f420", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1016,
+        "Name": "Question 7 on ___________________________", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "g1", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "g2", "IsAnswer": true },
+            { "Id": 1057, "QuestionId": 1010, "Name": "g3", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "g4", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1017,
+        "Name": "Question 8 on ___________________________", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "h1", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "h2", "IsAnswer": true },
+            { "Id": 1057, "QuestionId": 1010, "Name": "h3", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "h4", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1018,
+        "Name": "Question 9 on ___________________________", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "i1", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "i2", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "i3", "IsAnswer": true },
+            { "Id": 1058, "QuestionId": 1010, "Name": "i4", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1019,
+        "Name": "Question 10 on ___________________________", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "j1", "IsAnswer": true },
+            { "Id": 1056, "QuestionId": 1010, "Name": "j2", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "j3", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "j4", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    }]
+}
